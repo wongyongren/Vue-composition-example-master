10122bd211232da8dbcaa08fcc1a04f207e5ee8f
wongyongren <wongyongren@hotmail.com>
2021-01-30 10:46:08 +0800

    Initial commit

---
+++ src/composition/use-brew-list.js
@@ -0,0 +1,18 @@
+import {ref, toRefs, reactive} from '@vue/composition-api';
+import useFetch from './use-fetch';
+export default function() {
+  let breweries = reactive({list: [], error: null, fetching: false});
+  const val = ref('');
+  const submitted = async () => {
+
+    const {response, error, fetchData, fetching} = useFetch(
+      `https://api.openbrewerydb.org/breweries/?by_name=${val.value}`,
+      {}
+    );
+    fetchData();
+    breweries.list = response;
+    breweries.error = error;
+    breweries.fetching = fetching;
+  };
+  return {submitted, ...toRefs(breweries), val};
+}
